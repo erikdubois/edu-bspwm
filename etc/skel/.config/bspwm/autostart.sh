f26e13f4866c47cd93731f17bd8fc56f8ee72c34
Erik Dubois <erik.dubois@gmail.com>
2026-05-26 23:14:17 +0200

    update

--- etc/skel/.config/bspwm/autostart.sh
+++ etc/skel/.config/bspwm/autostart.sh
@@ -36,7 +36,7 @@ pamac-tray &
 xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-picom --config $HOME/.config/bspwm/picom.conf &
+fastcompmgr -c &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 #run volumeicon &
